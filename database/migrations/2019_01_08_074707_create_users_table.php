c3b753ef22fd5820737a7061c128af085f14d46d
kinrai92 <ray920213@gmail.com>
2019-01-11 15:03:11 +0900

    up

--- database/migrations/2019_01_08_074707_create_users_table.php
+++ database/migrations/2019_01_08_074707_create_users_table.php
@@ -15,20 +15,22 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('mail',100)->unique();
+            $table->string('mail', 100)->unique();
             $table->text('password');
+
             $table->unsignedInteger('mtb_user_status_id');
-            $table->string('token',20)->unique();
+            $table->string('token', 20)->unique();
+
             $table->text('lastname')->nullable();
             $table->text('firstname')->nullable();
             $table->text('lastname_reading')->nullable();
             $table->text('firstname_reading')->nullable();
             $table->unsignedInteger('mtb_area_id')->nullable();
             $table->text('address')->nullable();
-            $table->string('phone_no',11)->unique()->nullable();
+            $table->string('phone_no', 11)->unique()->nullable();
             $table->integer('gender_flg')->default('1')->comment('1:MALE 2:FEMALE')->nullable();
             $table->date('birthday')->nullable();
-            $table->string('nickname',40)->unique()->nullable();
+            $table->string('nickname', 40)->unique()->nullable();
             $table->softDeletes();
             $table->timestamps();
 
