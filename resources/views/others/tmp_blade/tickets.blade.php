a959fd78e55cd989ccb82c71166b26faabafc071
attilaCadabra <hjwkky4869@gmail.com>
2019-01-15 16:34:08 +0900

    add changes to tickets.blade etc

--- resources/views/others/tmp_blade/tickets.blade.php
+++ resources/views/others/tmp_blade/tickets.blade.php
@@ -32,51 +32,45 @@ div.current {
           <a href="/user_tickets/cancelled">キャンセル済み</a>
         </div>
       </div>
-
       @foreach($tickets as $ticket)
-        <div class="row top36 div01 bg-light">
-          <div class="col-sm-4 top24 text-center">
-            <img src="{{ asset('/storage/' . $ticket->event->picture1) }}" class="media-object" style="width:300px">
-          </div>
-          <div class="col-sm-8">
-            <div class="row top36">
-              <div class="col-sm-4 text-left">イベント名</div>
-              <div class="col-sm-8 text-right">{{ $ticket->event->title }}</div>
-            </div>
-            <div class="row top6">
-              <div class="col-sm-4 text-left">開催時間</div>
-              <div class="col-sm-8 text-right">
-                {{ $ticket->event->start_at->format("Y年n月j日 H時s分") }}
-              </div>
-            </div>
-            <div class="row top6">
-              <div class="col-sm-4 text-left">地域</div>
-              <div class="col-sm-8 text-right">{{ $ticket->event->mtb_municipality->value }}</div>
-            </div>
-            <div class="row top6">
-              <div class="col-sm-4 text-left">参加費用</div>
-              <div class="col-sm-8 text-right">{{ number_format($ticket->event->cost) }}円/人</div>
-            </div>
-            <div class="row top6">
-              <div class="col-sm-4 text-left">開催状態</div>
-              <div class="col-sm-8 text-right">
-                @if ($ticket->event->mtb_event_status_id == 3)
-                キャンセル済
-                @elseif ($ticket->event->mtb_event_status_id == 2 && $ticket->event->start_at >= \Carbon\Carbon::now())
-                未開催
-                @elseif ($ticket->event->mtb_event_status_id == 2 && $ticket->event->start_at < \Carbon\Carbon::now())
-                開催済
-                @endif
-              </div>
-            </div>
-            <div class="row top6">
-              <div class="col-sm-4 text-left">主催者</div>
-              <div class="col-sm-8 text-right">{{ $ticket->event->cooperation->name }}</div>
-            </div>
-
-          </div>
-        </div>
-      @endforeach
+      <div class="row borderln">
+         <div class="col-md-4">
+         <img src="{{asset('/storage/'.$ticket->event->picture1)}}" width="300px" height="230px" style="padding:20px">
+         </div>
+         <div class="col-md-8">
+           <table class="table">
+             <tr>
+               <td>チケットの番号:</td>
+               <td>{{$ticket->code}}</td>
+             </tr>
+             <tr>
+               <td>イベント:</td>
+               <td>$ticket->event->name</td>
+             </tr>
+             <tr>
+               <td>チケット状態:</td>
+               <td>$ticket->mtb_ticket_status</td>
+             </tr>
+            <tr>
+              <td>イベント開催日時:</td>
+              <td>$ticket->event->start_at</td>
+            </tr>
+            <tr>
+              <td>イベント開催場所:</td>
+              <td>$ticket->event->mtb_municipality->value</td>
+            </tr>
+            <tr>
+              <td>イベント主催者:</td>
+              <td>$ticket->event->cooperation->name</td>
+            </tr>
+            <tr>
+              <td>キャンセル:</td>
+              <td><a href="#">キャンセル</a></td>
+            </tr>
+           </table>
+         </div>
+      </div>
+    @endforeach
     </div>
 
     <div class="col-sm-2"></div>
