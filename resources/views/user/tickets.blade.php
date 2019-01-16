757b5f03b37fcc8cd47011febc2488a9042d5738
wodemima93112222 <tao15@c.kyoai.ac.jp>
2019-01-16 15:10:03 +0900

    merge srouce

--- resources/views/user/tickets.blade.php
+++ resources/views/user/tickets.blade.php
@@ -1,25 +1,23 @@
 @extends("layout.layout")
 @section('title','Tickets')
 @section("content")
+<style>
+div.current {
+  background: gray;
+}
+</style>
 
   <div class="col-sm text-center top36 bottom36">
     <p>チケット一覧</p>
   </div>
 
-  <div class="row top36 bottom36">
+  <div class="media row top36 bottom36">
     <div class="col-sm-2"></div>
 
     <div class="col-sm-8">
-        <nav class="navbar navbar-expand-sm">
-            <ul class="navbar-nav">
-                <li id="interval" class="nav-item {{ ($current_page=='all') ? 'current' : '' }}"><a class="nav-link navbar-brand" href="/user_tickets">すべて</a></li>
-                <li id="interval" class="nav-item {{ ($current_page=='not_used') ? 'current' : '' }}"><a class="nav-link navbar-brand" href="/user_tickets/not_used">未使用</a></li>
-                <li id="interval" class="nav-item {{ ($current_page=='used') ? 'current' : '' }}"><a class="nav-link navbar-brand" href="/user_tickets/used">使用済み</a></li>
-                <li id="interval" class="nav-item {{ ($current_page=='cancelled') ? 'current' : '' }}"><a class="nav-link navbar-brand" href="/user_tickets/cancelled">キャンセル済み</a></li>
-            </ul>
-　　　　 </nav>
 
-        <!-- 三元运算
+      <div class="row text-center">
+        <!-- 三元运算 -->
         <div class="col-sm-3 {{ ($current_page=='all') ? 'current' : '' }}">
           <a href="/user_tickets">すべて</a>
         </div>
@@ -31,46 +29,44 @@
         </div>
         <div class="col-sm-3 {{ ($current_page=='cancelled') ? 'current' : '' }}">
           <a href="/user_tickets/cancelled">キャンセル済み</a>
-        </div>-->
-      @foreach($tickets as $ticket)
-      <div class="row borderln">
-
-         <div class="col-md-8">
-           <table class="table">
-             <tr>
-               <td>QRコード:</td>
-               <td id="qrcode"></td>
-               <script type="text/javascript">
-                new QRCode(document.getElementById("qrcode"), {
-                 text: "{{$ticket->code}}",
-                 width: 100,
-                 height: 100
-               });
-               </script>
-             </tr>
-             <tr>
-               <td>イベント:</td>
-               <td>{{$ticket->event->title}}</td>
-             </tr>
-             <tr>
-               <td>チケット状態:</td>
-               <td>{{$ticket->mtb_ticket_status->value}}</td>
-             </tr>
-             <tr>
-               <td>user:</td>
-               <td>{{$ticket->user->nickname}}</td>
-             </tr>
-            <tr>
-              <td>キャンセル:</td>
-              <td><a href="#">キャンセル</a></td>
-            </tr>
-           </table>
-         </div>
+        </div>
       </div>
-    @endforeach
-    </div>
 
-    <div class="col-sm-2"></div>
-  </div>
-
-@endsection
+      @foreach($tickets as $ticket)
+      <div class="row top36 div01 bg-light">
+        <div class="col-sm-2 top24 text-center">
+        </div>
+        <div class="col-sm-9">
+          <div class="row top36">
+            <div class="col-sm-4 text-left">チケットの番号:</div>
+            <div class="col-sm-8 text-right">{{$ticket->code}}</div>
+          </div>
+          <div class="row top36">
+            <div class="col-sm-4 text-left">QRコード:</div>
+            <div  class="col-sm-8 text-right margin">
+            <img src="data:image/png;base64, {!! base64_encode(QrCode::format('png')->size(100)->generate('{{$ticket->code}}')) !!} ">
+            </div>
+          </div>
+          <div class="row top6">
+            <div class="col-sm-4 text-left">イベント:</div>
+            <div class="col-sm-8 text-right">
+              {{$ticket->event->title}}
+            </div>
+          </div>
+          <div class="row top6">
+            <div class="col-sm-4 text-left">チケット状態:</div>
+            <div class="col-sm-8 text-right">{{$ticket->mtb_ticket_status->value}}</div>
+          </div>
+          <div class="row top6">
+            <div class="col-sm-4 text-left">user:</div>
+            <div class="col-sm-8 text-right">{{$ticket->user->nickname}}</div>
+          </div>
+          <div class="row top6 bottom36">
+            <div class="col-sm-4 text-left"></div>
+            <div class="col-sm-8 text-right">
+            <a href="#">キャンセル</a>
+            </div>
+          </div>
+        </div>
+      </div>
+      @endforeach
